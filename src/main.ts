20e14f1677713628ae8ca18674c13e627c463981
gl4zis <maker64@inbox.ru>
2023-12-23 16:23:31 +0300

    logout, delete account buttons

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 import ToastService from 'primevue/toastservice';
+import ConfirmationService from 'primevue/confirmationservice';
 import 'primevue/resources/themes/md-light-indigo/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeflex/primeflex.min.css';
@@ -12,4 +13,5 @@ createApp(App)
     .use(router)
     .use(PrimeVue)
     .use(ToastService)
+    .use(ConfirmationService)
     .mount('#app')
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,11 @@
 <template>
+  <ConfirmDialog/>
   <CenterContent>
     <Card class="card">
       <template #header>
         <BackButton/>
-        <LogoutButton/>
+        <Button class="logout" label="Logout" @click="logoutConfirm"/>
+        <Button class="delete" label="Delete Account" severity="danger" @click="removeConfirm"/>
       </template>
       <template #title>
         <h2>Your Profile</h2>
@@ -33,6 +35,7 @@
 
 <script setup lang="ts">
 import CenterContent from "@/layouts/CenterContent.vue";
+import ConfirmDialog from 'primevue/confirmdialog';
 import Card from "primevue/card";
 import BackButton from "@/components/BackButton.vue";
 import Divider from 'primevue/divider';
@@ -45,9 +48,13 @@ import {useToast} from "primevue/usetoast";
 import ProfileUpdateComponent from "@/components/ProfileUpdateComponent.vue";
 import {profileStore} from "@/stores/profileStore";
 import {MessageResponse} from "@/modules/dtoInterfaces";
-import LogoutButton from "@/components/LogoutButton.vue";
+import {useConfirm} from "primevue/useconfirm";
+import {authStore} from "@/stores/authStore";
+import router from "@/router";
+import Button from "primevue/button";
 
 const toast = useToast()
+const confirm = useConfirm()
 
 const username: Ref<string | undefined> = ref(profileStore.profile?.username)
 const usernameValidation: Ref<string> = ref('')
@@ -56,6 +63,34 @@ let uniqueCheckId = 0
 
 const email: Ref<string | undefined> = ref(profileStore.profile?.email)
 const emailValidation: Ref<string> = ref('')
+
+function logoutConfirm(): void {
+  confirm.require({
+    message: 'Are you sure you want logout from your account?',
+    header: 'Logout',
+    accept: () => {
+      profileStore.resetProfile()
+      authStore.resetTokens()
+      router.push('/')
+    }
+  })
+}
+
+function removeConfirm(): void {
+  confirm.require({
+    message: 'Are you sure you want forever delete your account?',
+    header: 'Delete Account',
+    icon: 'pi pi-exclamation-triangle',
+    rejectClass: 'p-button-text p-button-text',
+    acceptClass: 'p-button-danger p-button-text',
+    accept: async () => {
+      await serverApi.deleteSelfAccount()
+      profileStore.resetProfile()
+      authStore.resetTokens()
+      await router.push('/')
+    }
+  })
+}
 
 function validateUsername(): void {
   clearTimeout(uniqueCheckId)
@@ -91,6 +126,18 @@ async function checkUsernameUniqueness(): Promise<void> {
   position: relative;
   width: 70%;
   text-align: center;
+
+  .logout {
+    position: absolute;
+    top: 15px;
+    left: 80px;
+  }
+
+  .delete {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+  }
 
   .content {
     display: flex;
